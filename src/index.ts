d367405c3301f10622de4083f7a542c5a06df923
deepesh-sr <220714100078@cutm.ac.in>
2025-09-15 15:45:49 +0530

    signin added

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import * as z from "zod";
 import dotenv from '@dotenvx/dotenvx'
 import User from "./database/db.js";
 import * as bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken'
+
+
 
 dotenv.config();
 
@@ -32,66 +35,112 @@ app.listen(3000, () => console.log("Listen on port 3000"));
 const user = z.object({
     username: z.string().min(3).max(10),
     password: z
-    .string()
-    .min(8)
-    .max(20)
-    .refine((password) => /[A-Z]/.test(password))
-    .refine((password) => /[a-z]/.test(password))
-    .refine((p) => /[0-9]/.test(p))
-    .refine((p) => /[!@#$%^&*]/.test(p))
+        .string()
+        .min(8)
+        .max(20)
+        .refine((password) => /[A-Z]/.test(password))
+        .refine((password) => /[a-z]/.test(password))
+        .refine((p) => /[0-9]/.test(p))
+        .refine((p) => /[!@#$%^&*]/.test(p))
 })
 
 
 app.post('/signup', async (req, res) => {
-   
-    try{
+
+    try {
 
         const result = user.safeParse(req.body);
         if (result.success) {
 
-            
-            
+
+
             let username = req.body.username;
             let password = req.body.password;
-            
-            const findUser =await User.findOne({
-                username : username
+
+            const findUser = await User.findOne({
+                username: username
             });
-            
-            if ( findUser){
+
+            if (findUser) {
                 res.status(403).json({
-                    "msg" : "User already exists"
+                    "msg": "User already exists"
                 })
             }
-            
-            const hashedPassword = await bcrypt.hash(password,3);
 
-             
-            const newuser =  new User({
-                username : username,
-                password : hashedPassword
+            const hashedPassword = await bcrypt.hash(password, 3);
+
+
+            const newuser = new User({
+                username: username,
+                password: hashedPassword
             })
-            
+
             const saveResult = await newuser.save();
-            if ( saveResult) {
-                res.status(200).json({"msg" : "Signed up"})
+            if (saveResult) {
+                res.status(200).json({ "msg": "Signed up" })
             }
-            
+
         } else {
-            
+
             console.log(result.error)
             res.status(411).json({
-                "msg" : "Error in inputs"
+                "msg": "Error in inputs"
             })
         }
-        
-    }catch(e){
+
+    } catch (e) {
         console.error(e);
         res.status(500).json({
-            "msg" : "server error"
+            "msg": "server error"
         })
     }
 
 
-    
+
+})
+
+app.post('/signin', async (req, res) => {
+try{
+
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const finduser = await User.findOne({
+        username: username
+    })
+
+    if (finduser) {
+
+        let comparePassword = await bcrypt.compare(password,finduser.password);
+        if ( comparePassword){
+
+            const jwtSecret = process.env.JWT_SECRET;
+            if (!jwtSecret) {
+                throw new Error("JWT_SECRET is required but not found in environment variables");
+            }
+            const token = jwt.sign({ username: finduser.username }, jwtSecret);
+            
+            // localStorage.setItem("token" , token);
+            
+            res.status(200).json({
+                "msg" : "Sign in done",
+                "token" : token
+            })
+        }else{
+            res.status(403).json({
+                "msg" : "Incorrect Password"
+            })
+        }
+    }else{
+        res.status(403).json({
+            "msg" : "user doesn;t exist"
+        })
+    }
+}catch(e){
+    console.error(e);
+    res.status(500).json({
+        "msg" : "internal server error"
+    })
+}
 })
